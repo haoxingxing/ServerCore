4e5ccbbaf29933b9ca6c78a455ccebfedf30d6ec
haoxingxing <mr.haoxx@gmail.com>
2019-05-19 19:53:26 +0800

    fix make_unique

--- core/root.h
+++ core/root.h
@@ -4,6 +4,10 @@
 #include <memory>
 #include <vector>
 #include "log.h"
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
 class variable;
 class domain;
 class root
